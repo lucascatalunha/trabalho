a8ac04f6c2add3c380269d1e0eaf78ccc19a72cb
Lucas <lucaslcatalunha@gmail.com>
2025-02-08 16:08:59 -0300

    program0012

--- program012.py
+++ program012.py
@@ -1,9 +1,9 @@
-def double_number(number: int) -> int:
-    return number * 2
+def square_number(number: int) -> int:
+    return number**2
 
 
 try:
     inputnum = int(input("Escreva um numero: "))
-    print(double_number(inputnum))
+    print(square_number(inputnum))
 except ValueError:
     print("Escreva um número por favor.")
