a7d8d8651d309b4bb8d07f4e9bbd0485aa08e6fe
Lucas <lucaslcatalunha@gmail.com>
2025-02-10 14:10:49 -0300

    new programs

--- program015.py
+++ program015.py
@@ -1,1 +1,11 @@
+def minutes_into_seconds(hours: int) -> int:
+    return hours * 60
 
+
+try:
+    inputminutes = input(
+        "Escreva um número de minutos quer queres converter em segundos: "
+    )
+    print(minutes_into_seconds(inputminutes))
+except ValueError:
+    print("Escreva um número por favor.")
--- program016.py
+++ program016.py
@@ -1,1 +1,10 @@
+def string_concatenation(str1: str, str2: str) -> str:
+    return str1 + str2
 
+
+try:
+    inputstr1 = str(input("Escreva um número: "))
+    inputstr2 = str(input("Escreva os disconto, (apenas o número ex: 50): "))
+    print(string_concatenation(inputstr1, inputstr2))
+except ValueError:
+    print("Escreva uma palavra por favor.")
